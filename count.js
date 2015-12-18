ae71cc17849b57d123d1b4ed28bb0a1416b48dac
Andrés Domínguez <andres_gomez@live.com.mx>
2015-12-17 20:38:14 -0600

    Correjí estilos y modifiqué un par de líneas en js

--- count.js
+++ count.js
@@ -1,16 +1,21 @@
+$(document).onload(getTime());
 function getTime() {
 	now = new Date();
-	y2k = new Date("Dec 25 2015 00:00:00");
-	days = (y2k - now) / 1000 / 60 / 60 / 24;
+	//fecha = new Date("Jan 25 2016 15:00:00");
+	fecha = new Date("Dec 25 2016 15:00:00");
+	days = (fecha - now) / 1000 / 60 / 60 / 24;
 	daysRound = Math.floor(days);
-	hours = (y2k - now) / 1000 / 60 / 60 - (24 * daysRound);
+	hours = (fecha - now) / 1000 / 60 / 60 - (24 * daysRound);
 	hoursRound = Math.floor(hours);
-	minutes = (y2k - now) / 1000 /60 - (24 * 60 * daysRound) - (60 * hoursRound);
+	minutes = (fecha - now) / 1000 /60 - (24 * 60 * daysRound) - (60 * hoursRound);
 	minutesRound = Math.floor(minutes);
-	seconds = (y2k - now) / 1000 - (24 * 60 * 60 * daysRound) - (60 * 60 * hoursRound) - (60 * minutesRound);
+	seconds = (fecha - now) / 1000 - (24 * 60 * 60 * daysRound) - (60 * 60 * hoursRound) - (60 * minutesRound);
 	secondsRound = Math.round(seconds);
 	if (daysRound <= "-1") {
-		//Si el conteo regresivo del script el valor de los días es mayor a -1 se para el script, ya que la fecha esperada se a cumplido, es necesaria este línea de código ya que si no se pone seguiria el conteo regresívo pero en valores negativos.
+		//   IMPORTANTE  //
+		//Si el conteo regresivo del script el valor de los días es mayor a -1 se para el script, 
+		//ya que la fecha esperada se a cumplido, es necesaria este línea de código ya que si no se pone 
+		//seguiria el conteo regresívo pero en valores negativos.
 	}
 	else{
 		document.getElementById('dias').innerHTML = daysRound;
@@ -20,4 +25,6 @@ function getTime() {
 	}
 	newtime = window.setTimeout("getTime();", 1000);
 }
+
+
 

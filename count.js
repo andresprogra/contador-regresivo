1ac90bacc20e7fe0229937d55599eff860ad9353
Andrés Domínguez <andres_gomez@live.com.mx>
2019-05-25 17:16:46 -0500

    Update count.js

--- count.js
+++ count.js
@@ -1,7 +1,7 @@
 $(document).onload(getTime());
 function getTime() {
 	now = new Date();
-	fecha = new Date("2016-08-09 23:46:48 UTC");
+	fecha = new Date("2016-12-25 00:00:00 UTC");
 	days = (fecha - now) / 1000 / 60 / 60 / 24;
 	daysRound = Math.floor(days);
 	hours = (fecha - now) / 1000 / 60 / 60 - (24 * daysRound);
